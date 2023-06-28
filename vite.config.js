5961a7fe619d117f5ff44a1ea1a11d60ce847680
Khanh Bui <kbuiblue@gmail.com>
2023-06-29 01:00:01 +0700

    Configured Vite and Sass to run concurrently

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,23 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [],
+    server: {
+        port: 5173,
+    },
+    css: {
+        devSourcemap: true, 
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@import "./src/scss/style.scss";`,
+            },
+        },
+    },
+    build: {
+        sourcemap: true,
+    },
+    optimizeDeps: {
+        entries: ["./index.html", "./src/index.js"],
+        include: ["sass"],
+    },
 });
+
