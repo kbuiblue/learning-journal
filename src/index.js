0fdb35792852b41681feb2b65e72edebd6982c12
Khanh Bui <kbuiblue@gmail.com>
2023-06-30 13:15:46 +0700

    Implemented View More button

--- src/index.js
+++ src/index.js
@@ -4,55 +4,74 @@ const appSettings = {
 };
 
 const app = firebase.initializeApp(appSettings);
-const database = firebase.database();
 const blogsInDB = firebase.database().ref("blogs-list");
 
 const heroEl = document.querySelector(".hero");
 const contentEl = document.querySelector(".content");
 
+const BLOGS_PER_PAGE = 3;
+
+let blogStartIndex = 0;
+let data;
+
 blogsInDB.on("value", function (snapshot) {
     if (snapshot.exists()) {
-        const data = Object.entries(snapshot.val());
-        renderHtml(data);
+        data = Object.entries(snapshot.val());
+        renderHtml(data, true);
     } else {
         console.log("No data available");
     }
 });
 
-function renderHtml(data) {
+document.addEventListener("click", function (e) {
+    if (e.target.classList.contains("view-more")) {
+        if (data.length > blogStartIndex) {
+            blogStartIndex++;
+        }
+
+        const partialData = data.slice(blogStartIndex);
+
+        if (partialData.length >= 1) {
+            renderHtml(partialData, false);
+        }
+    }
+});
+
+function renderHtml(data, isInitialRender) {
     let heroHtml = "";
     let contentHtml = "";
     let heroImageUrl = "";
 
-    data.forEach(function ([key, blog]) {
-        let html = `
+    data.every(function ([key, blog]) {
+        if (blogStartIndex === 0 && isInitialRender) {
+            heroHtml = `
             <div class="blog" id="${key}">
-                ${key === "blog1" ? '<div class="blog1-content">' : ""}
+                <div class="blog1-content">
+                    <p class="blog-date">${blog.date}</p>
+                    <h2 class="blog-title">${blog.title}</h2>
+                    ${renderSections(key, blog.sections)}
+                </div>
+            </div>
+        `;
+        } else {
+            contentHtml += `
+            <div class="blog" id="${key}">
                 <p class="blog-date">${blog.date}</p>
                 <h2 class="blog-title">${blog.title}</h2>
                 ${renderSections(key, blog.sections)}
-                ${key === "blog1" ? '</div>' : ""}
-            </div>
-        `;
-
-        const imageSection = blog.sections.find(
-            (section) => section.type === "image"
-        );
-
-        if (key === "blog1") {
-            heroHtml = html;
-
-            if (imageSection) {
-                heroImageUrl = imageSection.url;
-            }
-        } else {
-            contentHtml += html;
+            </div>`;
         }
 
-        heroEl.innerHTML = heroHtml;
+        if (key === "blog1" && isInitialRender) {
+            const imageSection = blog.sections.find(
+                (section) => section.type === "image"
+            );
 
-        const blog1El = document.getElementById("blog1");
-        if (blog1El) {
+            heroImageUrl = imageSection.url;
+            heroEl.innerHTML = heroHtml;
+
+            const blog1El = document.getElementById("blog1");
+
             blog1El.style.backgroundImage = `url(${heroImageUrl})`;
             blog1El.style.backgroundSize = "cover";
             blog1El.style.backgroundRepeat = "no-repeat";
@@ -61,7 +80,17 @@ function renderHtml(data) {
             blog1El.classList.add = "blog-image";
         }
 
-        contentEl.innerHTML = contentHtml;
+        if (
+            (blogStartIndex % BLOGS_PER_PAGE === 0 && blogStartIndex > 0) ||
+            data.length === 1
+        ) {
+            contentEl.innerHTML += contentHtml;
+            return false;
+        }
+
+        blogStartIndex++;
+
+        return true;
     });
 }
 
